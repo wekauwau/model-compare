825d0d34200747731ebfa3b8926e3a320b8dfc16
wekauwau <wekauwau@gmail.com>
2025-06-25 01:14:11 +0700

    add Price -ms

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,6 +10,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CarSeeder::class,
+            PriceSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/PriceSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PriceSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $data = [
+            [1, 1000, 1000, 1000],
+            [1, 1000, 1000, 1000],
+            [1, 1000, 1000, 1000],
+            [1, 1000, 1000, 1000],
+            [1, 1000, 1000, 1000],
+            [1, 1000, 1000, 1000],
+            [1, 1000, 1000, 1000],
+            [1, 1000, 1000, 1000],
+            [1, 1000, 1000, 1000],
+            [1, 1000, 1000, 1000],
+        ];
+    }
+}
