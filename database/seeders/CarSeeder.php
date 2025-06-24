079441464a3b3b8a48cad5685a321b6b7b032a14
wekauwau <wekauwau@gmail.com>
2025-06-25 01:32:34 +0700

    fix: seeder

--- database/seeders/CarSeeder.php
+++ database/seeders/CarSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Car;
 use Illuminate\Database\Seeder;
 
 class CarSeeder extends Seeder
@@ -20,5 +21,21 @@ class CarSeeder extends Seeder
             ['indianapolis', 'chevrolet', '8', 'gas', 43475, 'manual', 'rwd', 'convertible', 'silver', 'in', 16],
             ['winchester', 'volkswagen', '4', 'diesel', 83000, 'automatic', 'fwd', 'sedan', 'silver', 'va', 8],
         ];
+
+        foreach ($data as $record) {
+            Car::create([
+                'region' => $record[0],
+                'manufacturer' => $record[1],
+                'cylinders' => $record[2],
+                'fuel' => $record[3],
+                'odometer' => $record[4],
+                'transmission' => $record[5],
+                'drive' => $record[6],
+                'type' => $record[7],
+                'paint_color' => $record[8],
+                'state' => $record[9],
+                'age' => $record[10],
+            ]);
+        }
     }
 }
--- database/seeders/PriceSeeder.php
+++ database/seeders/PriceSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Price;
 use Illuminate\Database\Seeder;
 
 class PriceSeeder extends Seeder
@@ -20,5 +21,14 @@ class PriceSeeder extends Seeder
             [1, 1000, 1000, 1000],
             [1, 1000, 1000, 1000],
         ];
+
+        foreach ($data as $record) {
+            Price::create([
+                'car_id' => $record[0],
+                'rf' => $record[1],
+                'xgb' => $record[2],
+                'lgbm' => $record[3],
+            ]);
+        }
     }
 }
