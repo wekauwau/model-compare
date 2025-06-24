d12eef806ab2ff8c8f774d09bc5180a8da9bb492
wekauwau <wekauwau@gmail.com>
2025-06-25 00:10:29 +0700

    chore: layout.app add livewire

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,30 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8" />
 
-        <meta name="application-name" content="{{ config('app.name') }}" />
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+<head>
+    <meta charset="utf-8" />
 
-        <title>{{ config('app.name') }}</title>
+    <meta name="application-name" content="{{ config('app.name') }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <style>
-            [x-cloak] {
-                display: none !important;
-            }
-        </style>
+    <title>{{ config('app.name') }}</title>
 
-        @filamentStyles
-        @vite('resources/css/app.css')
-    </head>
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
+    </style>
 
-    <body class="antialiased">
-        {{ $slot }}
+    @filamentStyles
+    @livewireStyles
+    @vite('resources/css/app.css')
+</head>
 
-        @livewire('notifications')
+<body class="antialiased">
+    {{ $slot }}
 
-        @filamentScripts
-        @vite('resources/js/app.js')
-    </body>
+    @livewire('notifications')
+
+    @filamentScripts
+    @livewireScripts
+    @vite('resources/js/app.js')
+</body>
+
 </html>
