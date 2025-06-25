6de4f795348739e5a0a53b93a1a19dd58ef43193
wekauwau <wekauwau@gmail.com>
2025-06-25 22:45:24 +0700

    modularize

---
+++ resources/views/components/filament/modals/view-calculation/card.blade.php
@@ -0,0 +1,73 @@
+<div {{ $attributes->merge(['class' => 'mt-5 p-2']) }}>
+    <div class="mb-5 text-xl font-semibold text-blue-600">{{ $title }}</div>
+    <table class="text-lg">
+        <tr>
+            <td>MAE</td>
+            <td>=</td>
+            <td>
+                |A − P|
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>=</td>
+            <td>
+                <span class="font-semibold">
+                    |{{ number_format($actual, 2) }} − {{ number_format($predicted, 2) }}|
+                </span>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>=</td>
+            <td>
+                <span class="font-semibold text-red-600">
+                    {{ number_format($mae, 2) }}
+                </span>
+            </td>
+        <tr>
+            <td>MAPE</td>
+            <td>=</td>
+            <td>
+                |A − P| &divide; A &times; 100%
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>=</td>
+            <td>
+                <span class="font-semibold">
+                    |{{ number_format($actual, 2) }} − {{ number_format($predicted, 2) }}|
+                </span>
+                &divide;
+                {{ number_format($actual, 2) }}
+                &times;
+                100%
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>=</td>
+            <td>
+                <span class="font-semibold"><span class="text-red-600">{{ number_format($mapeStep1, 2) }}</span>
+                    &divide;
+                    {{ number_format($actual, 2) }}</span> &times;
+                100%
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>=</td>
+            <td>
+                <span class="font-semibold">{{ number_format($mapeStep2, 4) }} &times; 100%</span>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>=</td>
+            <td>
+                <span class="font-semibold text-purple-600">{{ number_format($mape, 2) }}%</span>
+            </td>
+        </tr>
+    </table>
+</div>
--- resources/views/filament/modals/view-calculation.blade.php
+++ resources/views/filament/modals/view-calculation.blade.php
@@ -1,7 +1,7 @@
 @php
     $actual = $car->dataset_price;
-    $predicted_rf = $car->predictedPrice->rf;
 
+    $predicted_rf = $car->predictedPrice->rf;
     $mae_rf = abs($actual - $predicted_rf);
     $mape_rf_step_1 = $mae_rf;
     $mape_rf_step_2 = $mape_rf_step_1 / $actual;
@@ -12,77 +12,6 @@
     <div>A = Actual price</div>
     <div>P = Predicted price</div>
 
-    <div class="mt-5 p-2 bg-blue-50">
-        <div class="mb-5 text-xl font-semibold text-blue-600">Random Forest</div>
-        <table class="text-lg">
-            <tr>
-                <td>MAE</td>
-                <td>=</td>
-                <td>
-                    |A − P|
-                </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>=</td>
-                <td>
-                    <span class="font-semibold">
-                        |{{ number_format($actual, 2) }} − {{ number_format($predicted_rf, 2) }}|
-                    </span>
-                </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>=</td>
-                <td>
-                    <span class="font-semibold text-red-600">
-                        {{ number_format($mae_rf, 2) }}
-                    </span>
-                </td>
-            <tr>
-                <td>MAPE</td>
-                <td>=</td>
-                <td>
-                    |A − P| &divide; A &times; 100%
-                </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>=</td>
-                <td>
-                    <span class="font-semibold">
-                        |{{ number_format($actual, 2) }} − {{ number_format($predicted_rf, 2) }}|
-                    </span>
-                    &divide;
-                    {{ number_format($actual, 2) }}
-                    &times;
-                    100%
-                </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>=</td>
-                <td>
-                    <span class="font-semibold"><span class="text-red-600">{{ number_format($mape_rf_step_1, 2) }}</span>
-                        &divide;
-                        {{ number_format($actual, 2) }}</span> &times;
-                    100%
-                </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>=</td>
-                <td>
-                    <span class="font-semibold">{{ number_format($mape_rf_step_2, 4) }} &times; 100%</span>
-                </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>=</td>
-                <td>
-                    <span class="font-semibold text-purple-600">{{ number_format($mape_rf, 2) }}%</span>
-                </td>
-            </tr>
-        </table>
-    </div>
+    <x-filament.modals.view-calculation.card title="Random Forest" :$actual :predicted="$predicted_rf" :mae="$mae_rf"
+        :mape-step-1="$mae_rf" :mape_step_2="$mape_rf_step_2" :mape="$mape_rf" class="bg-blue-50" />
 </div>
