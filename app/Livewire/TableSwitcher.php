3fcc541b0f28a3097468061923b0b7ed297b34cd
wekauwau <wekauwau@gmail.com>
2025-06-28 15:47:34 +0700

    refactor: default TableSwitcher $mode to 'input'

--- app/Livewire/TableSwitcher.php
+++ app/Livewire/TableSwitcher.php
@@ -6,7 +6,7 @@ use Livewire\Component;
 
 class TableSwitcher extends Component
 {
-    public string $mode = 'sample';
+    public string $mode = 'input';
 
     public function setMode($value)
     {
