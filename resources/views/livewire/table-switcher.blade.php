a89b17d0af213e4f7431ace88eed889ba046198a
wekauwau <wekauwau@gmail.com>
2025-06-28 19:11:55 +0700

    fix: border for table switcher

--- resources/views/livewire/table-switcher.blade.php
+++ resources/views/livewire/table-switcher.blade.php
@@ -2,11 +2,11 @@
     {{-- Switch Buttons --}}
     <div class="flex justify-center mt-10 mb-6 space-x-2">
         <button wire:click="setMode('input')"
-            class="px-4 py-2 border-2 border-gray-400 rounded-lg hover:bg-blue-500 hover:border-blue-500 hover:text-white {{ $mode === 'input' ? 'bg-blue-500 border-blue-500 text-white' : 'bg-white' }}">
+            class="px-4 py-2 border-2 rounded-lg hover:bg-blue-500 hover:border-blue-500 hover:text-white {{ $mode === 'input' ? 'bg-blue-500 border-blue-500 text-white' : 'bg-white border-gray-400 ' }}">
             Input
         </button>
         <button wire:click="setMode('sample')"
-            class="px-4 py-2 border-2 border-gray-400 rounded-lg hover:bg-blue-500 hover:border-blue-500 hover:text-white {{ $mode === 'sample' ? 'bg-blue-500 border-blue-500 text-white' : 'bg-white' }}">
+            class="px-4 py-2 border-2 rounded-lg hover:bg-blue-500 hover:border-blue-500 hover:text-white {{ $mode === 'sample' ? 'bg-blue-500 border-blue-500 text-white' : 'bg-white border-gray-400 ' }}">
             Sample
         </button>
     </div>
